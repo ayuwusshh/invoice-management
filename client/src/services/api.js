3460b412e286f1d6a714077e0a7bfc45fd52c2b7
Ayush Kumar Singh <iamayushsingh02@gmail.com>
2025-10-20 00:42:17 +0530

    changed api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL+'/api';
 const api = axios.create({
     baseURL: API_BASE_URL,
     headers: {
@@ -10,6 +10,8 @@ export const getConfig = (token) => {
     return {
         headers: {
             Authorization: `Bearer ${token}`,
-        },};
+        },
+    };
 };
 export default api;
+API_BASE_URL
